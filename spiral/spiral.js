867d76d73008a204d9e5de966b66ee355e431adf
Alex Boisvert <boisvert42@gmail.com>
2022-01-30 16:53:17 -0800

    Initial take

--- spiral/spiral.js
+++ spiral/spiral.js
@@ -5,7 +5,7 @@ function reverseString(str) {
 
 function new_word_options(forward_words, backward_words) {
   // The maximum number of words to return
-  MAX_RET_WORDS = 20;
+  MAX_RET_WORDS = 15;
   // Find the words we've already used
   var used_words = new Set(forward_words.concat(backward_words));
   // Get the last word
@@ -16,7 +16,7 @@ function new_word_options(forward_words, backward_words) {
   var this_str;
   // Switch to "begin" depending on new_len
   if (new_len < 0) {
-    this_word = backward_words[backward_words.length - 1];
+    this_word = backward_words[0];
     new_len = -1 * new_len;
     this_dict = window.spiralData['begin'];
     this_str = reverseString(this_word.substr(0, new_len));
@@ -31,10 +31,10 @@ function new_word_options(forward_words, backward_words) {
   var ret2 = [];
   // Remove anything that's already been used
   ret.forEach(function (r) {
-    var good_word = True
-    r.forEach(function(w) {
-      if used_words.has(w) {
-        good_word = False;
+    var good_word = true
+    r['words'].forEach(function(w) {
+      if (used_words.has(w)) {
+        good_word = false;
       }
     });
     if (good_word) {
@@ -65,3 +65,53 @@ function add_word(forward_words, backward_words, this_word) {
 }
 
 /* Process the words in the textareas and write results */
+function processTextAreas() {
+  var forward_words = document.getElementById('inwardWords').value.split('\n');
+  var backward_words = document.getElementById('outwardWords').value.split('\n');
+
+  // get our options for the next word
+  var nwo = new_word_options(forward_words, backward_words);
+  var html = '';
+  nwo.forEach(function (nw) {
+    var id = JSON.stringify(nw['words']);
+    var thisText = nw['words'][0];
+    if (nw['words'][1]) {
+      thisText += ' / ' + nw['words'][1];
+    }
+    thisText += ` [${nw['leftover']}]`;
+    html += `
+    <label class="wordSelectorLabel">
+      <input type="radio" id='${id}' name="wordSelector" value='${id}'>
+      <span class="label-body">${thisText}</span>
+    </label>\n`;
+  });
+  // add the button
+  html += `<button class="button-primary" type="submit" onclick="processSelectedItem()">Add Selected Word(s)</button>`;
+  document.getElementById('possibles').innerHTML = html;
+  return false;
+}
+
+/* Process the selected item from the list */
+function processSelectedItem() {
+  // grab the words from the textareas
+  var forward_words = document.getElementById('inwardWords').value.split('\n');
+  var backward_words = document.getElementById('outwardWords').value.split('\n');
+  // Find out which radio button is checked
+  const radioButtons = document.querySelectorAll('input[name="wordSelector"]');
+  var selectedValue;
+  for (const radioButton of radioButtons) {
+      if (radioButton.checked) {
+          selectedValue = radioButton.value;
+          break;
+      }
+  }
+  console.log(selectedValue);
+  var this_word = JSON.parse(selectedValue);
+  var fb_words = add_word(forward_words, backward_words, this_word);
+  // Replace the values in the text areas
+  document.getElementById('inwardWords').value = fb_words[0].join('\n');
+  document.getElementById('outwardWords').value = fb_words[1].join('\n');
+  // Repeat the process
+  processTextAreas();
+  return true;
+}
