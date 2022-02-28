aed4e86495330d1f2668bc9a22e6ef645f1e4829
Alex Boisvert <boisvert42@gmail.com>
2022-02-27 22:36:46 -0800

    finalize

--- snake_charmer/sc_generate.py
+++ snake_charmer/sc_generate.py
@@ -19,9 +19,11 @@ MIN_SCORE = 50
 # The word list to use
 WORDLIST = 'stwl.txt'
 
-#%% Helper functions
+# %% Helper functions
 
 # Make partitions of a string
+
+
 def multiSlice(s, cutpoints):
     """
     Helper function for allPartitions
@@ -36,19 +38,21 @@ def multiSlice(s, cutpoints):
         return multislices
 
 # This includes partitions of length 0
+
+
 def allPartitions(s, num=None):
     n = len(s)
-    cuts = list(range(0,n+1))
+    cuts = list(range(0, n+1))
     if num:
         num_arr = [num-1]
     else:
         num_arr = range(n)
     for k in num_arr:
-        for cutpoints in itertools.combinations_with_replacement(cuts,k):
-            yield multiSlice(s,cutpoints)
+        for cutpoints in itertools.combinations_with_replacement(cuts, k):
+            yield multiSlice(s, cutpoints)
 
 
-#%% Read in word list
+# %% Read in word list
 all_words = set()
 beginnings = set()
 ends = set()
@@ -56,7 +60,7 @@ all_word_dict = dict()
 
 with open(WORDLIST, 'r') as fid:
     for line in fid:
-        word, score = line.split(';')
+        word, score = line.upper().split(';')
         score = int(score)
         if score >= MIN_SCORE and len(word) >= MIN_WORD_LENGTH:
             all_words.add(word)
@@ -67,7 +71,7 @@ with open(WORDLIST, 'r') as fid:
                 beginnings.add(w1)
                 ends.add(w2)
 
-#%% Create needed dictionaries
+# %% Create needed dictionaries
 prev_word_count = 1e6
 new_word_count = 0
 good_words = all_words.copy()
@@ -110,10 +114,10 @@ for word in all_words:
 
 print(len(good_words))
 
-#%% Make one global dictionary from this and serialize into JSON format
+# %% Make one global dictionary from this and serialize into JSON format
 
 # The default word score (mostly for missing words)
-DEF_WORD_SCORE = MIN_SCORE
+DEF_WORD_SCORE = 0.85 * MIN_SCORE
 
 helper_dict = dict()
 items = {'begin': begin_dict, 'end': end_dict}
@@ -123,43 +127,41 @@ for name, d in items.items():
         helper_dict[name][_str] = []
         for this_word in this_set:
             w0, w1 = this_word
-            score = all_word_dict.get(w0, DEF_WORD_SCORE) + all_word_dict.get(w1, DEF_WORD_SCORE)
+            score = all_word_dict.get(
+                w0, DEF_WORD_SCORE) + all_word_dict.get(w1, DEF_WORD_SCORE)
             leftover_len = len(w0) - len(_str)
             if w1 is not None:
                 leftover_len -= len(w1)
             if name == 'begin':
-                leftover = w0[-leftover_len:][::-1]
+                leftover = w0[-leftover_len:]
             else:
-                leftover = w0[:leftover_len][::-1]
+                leftover = w0[:leftover_len]
             d2 = {'words': [w0, w1], 'score': score, 'leftover': leftover}
             helper_dict[name][_str].append(d2)
 
 # Write out this file for JavaScript purposes
 with open('helper_dict.json', 'w') as fid:
-    json.dump(helper_dict, fid)            
+    json.dump(helper_dict, fid)
 
 
-#%% Functions for the main loop
+# %% Functions for the main loop
 
 # Number of results to show
 RESULT_WORDS = 20
 
-def new_word_options(forward_words, backward_words):
-    used_words = set(forward_words + backward_words)
-    this_word = forward_words[-1]
-    new_len = len(''.join(forward_words)) - len(''.join(backward_words))
-    this_dict = helper_dict['end']
+def new_word_options(loop1, loop2):
+    used_words = set(loop1 + loop2)
+    this_word = loop1[-1]
+    new_len = len(''.join(loop1)) - len(''.join(loop2))
+    this_dict = helper_dict['begin']
     if new_len < 0:
-        this_word = backward_words[0]
+        this_word = loop2[-1]
         new_len = -1 * new_len
-        this_dict = helper_dict['begin']
-        this_str = this_word[:new_len][::-1]
-    else:
-        this_str = this_word[-new_len:][::-1]
+    this_str = this_word[-1 * new_len:]
     ret = this_dict[this_str]
     ret2 = []
     # remove anything that's already been used
-    for r in sorted(ret, key=lambda x:x['score'], reverse=True):
+    for r in sorted(ret, key=lambda x: x['score'], reverse=True):
         good_word = True
         for w in r:
             if w in used_words:
@@ -170,36 +172,38 @@ def new_word_options(forward_words, backward_words):
             return ret2
     return ret2
 
-def add_word(forward_words, backward_words, this_word):
-    new_len = len(''.join(forward_words)) - len(''.join(backward_words))
+
+def add_word(loop1, loop2, this_word):
+    new_len = len(''.join(loop1)) - len(''.join(loop2))
     w0, w1 = this_word
-    if new_len > 0: # add "main" word to backwards words
-        backward_words = [w0] + backward_words
+    if new_len > 0:  # add "main" word to loop2
+        loop2.append(w0)
         if w1:
-            forward_words.append(w1)
+            loop1.append(w1)
     else:
-        forward_words.append(w0)
+        loop1.append(w0)
         if w1:
-            backward_words = [w1] + backward_words
-    return forward_words, backward_words
+            loop2.append(w1)
+    return loop1, loop2
+
 
 def remove_last_word(forward_words, backward_words):
     pass
 
 #%% The main loop
 
-forward_words = ['SENEGAL']
-backward_words = ['GENES']
+loop1 = ['INEVITABLE']
+loop2 =  ['IN', 'EVITA']
 
 while True:
-    nwo = new_word_options(forward_words, backward_words)
+    nwo = new_word_options(loop1, loop2)
     for nw in nwo:
         print(nw['words'], nw['leftover'])
     _input = input().strip().upper().split(',')
     if len(_input) == 1:
         _input = [_input[0], None]
-    forward_words, backward_words = add_word(forward_words, backward_words, _input)
+    loop1, loop2 = add_word(loop1, loop2, _input)
 
-    print('forward_words = ' + str(forward_words))
-    print('backward_words = ' + str(backward_words))
-    print(len(''.join(forward_words)))
+    print('loop1 = ' + str(loop1))
+    print('loop2 =  ' + str(loop2))
+    print(len(''.join(loop1)))
